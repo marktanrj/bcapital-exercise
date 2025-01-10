d3f35fcc9da8fd5290e7a681720a8a7e0cc0c098
Mark Tan <marktanrj@gmail.com>
2025-01-10 08:42:39 +0800

    feat: add toast ui component

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ReactQueryClientProvider } from "../providers/query-client";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReactQueryClientProvider>
-          {children}
+          <main>{children}</main>
+          <Toaster />
         </ReactQueryClientProvider>
       </body>
     </html>
